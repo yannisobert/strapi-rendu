683405a03f21018e0ae7006d8f3761a14c1c1925
yannisobert <90550556+yannisobert@users.noreply.github.com>
2023-02-28 13:58:55 +0100

    strapi-training: Add files

---
+++ config/middlewares.js
@@ -0,0 +1,22 @@
+module.exports = [
+  'strapi::errors',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        directives: {
+          'default-src': ["'self'"],
+          'img-src': ["'self'", 'data:', 'blob:', `${process.env.SUPABASE_API_URL}`],
+        }
+      }
+    }
+  },
+  'strapi::cors',
+  'strapi::poweredBy',
+  'strapi::logger',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
+];
---
+++ config/plugins.js
@@ -0,0 +1,31 @@
+module.exports = ({ env }) => ({
+  upload: {
+    config: {
+      provider: 'strapi-provider-upload-supabase',
+      providerOptions: {
+        apiUrl: env('SUPABASE_API_URL'),
+        apiKey: env('SUPABASE_API_KEY'),
+        bucket: env('SUPABASE_BUCKET'),
+        directory: env('SUPABASE_DIRECTORY'),
+        options: {}
+      },
+      breakpoints: {
+        xlarge: 1920,
+        large: 1000,
+        medium: 750,
+        small: 500,
+        xsmall: 64,
+      },
+    },
+  },
+  transformer: {
+    enabled: true,
+    config: {
+      prefix: "/api/",
+      responseTransforms: {
+        removeAttributesKey: true,
+        removeDataKey: true,
+      },
+    },
+  },
+})
