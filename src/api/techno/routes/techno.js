83dd32bdd89541e10af90c70ec989672240ddf85
yannisobert <90550556+yannisobert@users.noreply.github.com>
2023-03-01 12:28:27 +0100

    strapi-training: Add api techno

---
+++ src/api/techno/routes/techno.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * techno router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::techno.techno');
---
+++ src/api/techno/services/techno.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * techno service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::techno.techno');
